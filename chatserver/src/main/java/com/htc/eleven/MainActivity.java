fa1115bb046a96d446952648603b8ab411f428f1
Eleven_Xiang <eleven.xiang@gmail.com>
2017-09-24 01:17:31 +0800

    add chat server module, which is empty yet.

---
+++ chatserver/src/main/java/com/htc/eleven/MainActivity.java
@@ -0,0 +1,15 @@
+package com.htc.eleven;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eleven_xianghtc.chatserver.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
